5e3d56c458dc3b79903d9b022c0e95592aa29cca
Tim Roes <mail@timroes.de>
2015-03-17 00:36:27 +0100

    fix: broken hotkey across browsers

--- src/main/frontend/scripts/directives/clickHotkey.js
+++ src/main/frontend/scripts/directives/clickHotkey.js
@@ -1,30 +1,11 @@
 angular.module('gp')
 .directive('clickHotkey', function($document) {
 
-	var MODIFIERS = ['shift', 'ctrl', 'meta', 'alt'];
-
 	return {
 		restrict: 'A',
-		scope: {
-			'clickHotkey': '@'
-		},
 		link: function(scope, elem) {
-			var hotkey = scope.clickHotkey.split('+');
-			var key = hotkey[hotkey.length - 1];
-			
-			
 			function isHotkey(ev) {
-				for(var i = 0; i < hotkey.length - 1; i++) {
-					if(MODIFIERS.indexOf(hotkey[i]) === -1) {
-						throw new Error('Modifier ' + hotkey[i] + ' does not exist.');
-					}
-					
-					if(!ev[hotkey[i] + 'Key']) {
-						return false;
-					}
-					
-				}
-				return key.toLowerCase() === ev.keyIdentifier.toLowerCase();
+				return ev.ctrlKey && (ev.which === 13 || ev.which === 10);
 			}
 			
 			$document.bind('keypress', function(ev) {
